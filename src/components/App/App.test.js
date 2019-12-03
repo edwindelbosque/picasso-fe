6f36a4ba79aa6f801c0dac9ec8e4728c9e676bdc
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-03 14:56:33 -0700

    Set up boiler-plates and install necessary npm packages

---
+++ src/components/App/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = shallow(<App />);
+	});
+
+	it('App should match snapshot', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+});
