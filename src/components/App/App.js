d4228f44fc1ad98f26c4a1d4787c1783c79b7dec
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-08 21:06:29 -0700

    Add NavBar initial styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
+import NavBar from '../NavBar/NavBar';
 
 class App extends Component {
 	constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<p>Picasso</p>
+				<NavBar />
 			</div>
 		);
 	}
