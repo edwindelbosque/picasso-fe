c47a05f81441d2c404a76123e4794bdb612e9b90
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-11 13:01:02 -0700

    Update App snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -9,7 +9,7 @@ describe('App', () => {
 		wrapper = shallow(<App />);
 	});
 
-	it.skip('App should match snapshot', () => {
+	it('App should match snapshot', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
 });
