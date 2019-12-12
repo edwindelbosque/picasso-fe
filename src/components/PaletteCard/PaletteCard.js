69a39bf83980b49c06da4a42d2b2cc2bf3ce64c7
Matthew Malone <m.malone@me.com>
2019-12-12 06:46:31 -0700

    Append the palettes with colors and names to the DOM

--- src/components/PaletteCard/PaletteCard.js
+++ src/components/PaletteCard/PaletteCard.js
@@ -3,20 +3,23 @@ import './PaletteCard.scss';
 import Fade from 'react-reveal/Fade';
 
 const PaletteCard = ({ menuIsActive, palette }) => {
-	return (
-		<Fade when={menuIsActive} duration={600} delay={150}>
-			<div className='PaletteCard'>
-				<h4>{palette.paletteName}</h4>
-				<div className='colors'>
-					<div style={{ background: `#${palette.color1}` }}></div>
-					<div style={{ background: `#${palette.color2}` }}></div>
-					<div style={{ background: `#${palette.color3}` }}></div>
-					<div style={{ background: `#${palette.color4}` }}></div>
-					<div style={{ background: `#${palette.color5}` }}></div>
+	if (palette.id) {
+		const colorForPalette = palette.colors.map((color, i) => {
+			return <div key={i} style={{ background: color.hex.value }}></div>;
+		});
+		return (
+			<Fade when={menuIsActive} duration={600} delay={150}>
+				<div className='PaletteCard'>
+					<h4>{palette.paletteName}</h4>
+					<div className='colors'>{colorForPalette}</div>
 				</div>
-			</div>
-		</Fade>
-	);
+			</Fade>
+		);
+	} else {
+		return (
+			<> </>
+		)
+	}
 };
 
 export default PaletteCard;
--- src/components/Palettes/Palettes.js
+++ src/components/Palettes/Palettes.js
@@ -4,8 +4,6 @@ import PaletteCard from '../PaletteCard/PaletteCard';
 import { Link } from 'react-router-dom';
 
 const Palettes = ({ menuIsActive, palettes, toggleMenu }) => {
-	console.log('PALETTES IS WORKING!!', palettes);
-	
 	return (
 		<section className='Palettes'>
 			<h2>Palettes</h2>
