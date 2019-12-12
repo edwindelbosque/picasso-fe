c1bd78ab2e6b93961dacc3e02c0c6aa73f73a93e
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-11 19:23:58 -0700

    Improve main palette generator styling

--- src/components/RandomPalette/RandomPalette.js
+++ src/components/RandomPalette/RandomPalette.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './RandomPalette.scss';
 
-const RandomPalette = ({palette}) => {
+const RandomPalette = ({ palette }) => {
 	if (!palette.length) {
-		return (<></>)
+		return <></>;
 	} else {
-		const createdColor = palette.map( (color, i) => {
-			return (<div key={i} style={ {background: color.hex.value}}></div>)
-		})
+		const createdColor = palette.map((color, i) => {
+			return <div key={i} style={{ background: color.hex.value }}></div>;
+		});
+		const createdInfo = palette.map((color, i) => {
+			return (
+				<div key={i}>
+					<h5>{color.name.value}</h5>
+					<p>{color.hex.value}</p>
+				</div>
+			);
+		});
 		return (
 			<div>
 				<div className='random-palette-card'>
 					<h4>{palette.paletteName}</h4>
-					<div className='colors-holder'>
-						{createdColor}
-					</div>
+					<div className='colors-holder'>{createdColor}</div>
 				</div>
+				<div className='colors-info'>{createdInfo}</div>
 			</div>
 		);
 	}
-
 };
 
-export default RandomPalette
+export default RandomPalette;
